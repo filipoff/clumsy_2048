ce2d20efede2fff65fe1b43bdf853a2f99830157
Martin <marto_1337@hotmail.com>
2015-06-09 03:57:29 +0300

    using array again, almost fix left_slide

--- grid.py
+++ grid.py
@@ -8,10 +8,7 @@ class Grid:
         self.width = width
         self.height = height
         self.used_cell_positions = []
-        self.cells = {}
-        for x in range(height):
-            for y in range(width):
-                self.cells[(x, y)] = 0
+        self.cells = [[0 for x in range(width)] for y in range(height)]
 
     def __getitem__(self, index):
         return self.cells[index]
@@ -19,7 +16,7 @@ class Grid:
     def draw(self):
         for x in range(self.height):
             for y in range(self.width):
-                print(self.cells[(x, y)], end=' ')
+                print(self.cells[x][y], end=' ')
             print()
         print()
 
@@ -33,11 +30,26 @@ class Grid:
             random_y = randint(0, self.width - 1)
 
         random_value = 2 if random() < 0.9 else 4
-        self.cells[(random_x, random_y)] = random_value
+        self.cells[random_x][random_y] = random_value
         self.used_cell_positions.append((random_x, random_y))
 
     def slide_left(self):
-        pass
+
+        def merge(sequence):
+            result = []
+            for x in range(len(sequence) - 1):
+                for y in range(x + 1, len(sequence)):
+                    if sequence[x] == sequence[y] and sequence[x] != 0:
+                        result.append(sequence[x] * 2)
+                        sequence[y] = 0
+                        sequence[x] *= 2
+            zeroes_count = self.width - len(result)
+            for _ in range(zeroes_count):
+                result.append(0)
+            return result
+
+        for row in range(self.height):
+            self.cells[row] = merge(self.cells[row])
 
     def slide_right(self):
         pass
@@ -53,7 +65,10 @@ if __name__ == '__main__':
     width = 4
     height = 4
     g = Grid(width, height)
-    g.generate_number()
+    g.cells[0][0] = 2
+    g.cells[0][1] = 2
+    g.cells[0][2] = 2
+    #g.cells[0][3] = 2
     g.draw()
     g.slide_left()
     g.draw()

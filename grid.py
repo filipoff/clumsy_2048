ca729df1779000e5d8d5739c933af732c105b749
Martin <marto_1337@hotmail.com>
2015-06-17 20:09:47 +0300

    added reset, dimensions and __getitem__ methods

--- grid.py
+++ grid.py
@@ -4,13 +4,18 @@ from random import randint, choice, random
 class Grid:
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         self.__cells = [[0 for x in range(width)] for y in range(height)]
 
+    def __getitem__(self, position):
+        x, y = position
+        if x < self.__width and x >= 0 and y < self.__height and y >= 0:
+            return self.__cells[x][y]
+
     def draw(self):
-        for x in range(self.height):
-            for y in range(self.width):
+        for x in range(self.__height):
+            for y in range(self.__width):
                 print('[{}]'.format(self.__cells[x][y]), end=' ')
             print()
         print()
@@ -18,11 +23,18 @@ class Grid:
     @property
     def free_cells(self):
         result = []
-        for x in range(self.height):
-            for y in range(self.width):
+        for x in range(self.__height):
+            for y in range(self.__width):
                 if self.__cells[x][y] == 0:
                     result.append((x, y))
         return result
+
+    def reset(self):
+        self.__cells = [
+            [0 for x in range(self.__width)] for y in range(self.__height)]
+
+    def dimensions(self):
+        return self.__width, self.__height
 
     def generate_number(self):
         if len(self.free_cells) == 0:
@@ -34,36 +46,36 @@ class Grid:
 
     def slide_left(self):
         points_recieved = 0
-        for row_index in range(self.height):
+        for row_index in range(self.__height):
             points_recieved += self.__left_merge(self.__cells[row_index])
         return points_recieved
 
     def slide_right(self):
         points_recieved = 0
-        for row_index in range(self.height):
+        for row_index in range(self.__height):
             points_recieved += self.__right_merge(self.__cells[row_index])
         return points_recieved
 
     def slide_up(self):
         points_recieved = 0
-        for column_index in range(self.width):
+        for column_index in range(self.__width):
             column = [self.__cells[x][column_index]
-                      for x in range(self.height)]
+                      for x in range(self.__height)]
             points_recieved += self.__left_merge(column)
             self.__set_column(column_index, column)
         return points_recieved
 
     def slide_down(self):
         points_recieved = 0
-        for column_index in range(self.width):
+        for column_index in range(self.__width):
             column = [self.__cells[x][column_index]
-                      for x in range(self.height)]
+                      for x in range(self.__height)]
             points_recieved += self.__right_merge(column)
             self.__set_column(column_index, column)
         return points_recieved
 
     def __set_column(self, index, sequence):
-        for x in range(self.height):
+        for x in range(self.__height):
             self.__cells[x][index] = sequence[x]
 
     def __remove_zeros(self, sequence):

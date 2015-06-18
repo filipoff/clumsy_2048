6d2bb4b4527d23f18ca6229ce56f88aa09e4f146
Martin <marto_1337@hotmail.com>
2015-06-18 12:46:33 +0300

    developing the players chart class

--- game.py
+++ game.py
@@ -1,4 +1,30 @@
-from grid import Grid
+from enum import Enum
+
+
+class State(Enum):
+    running = 'running'
+    over = 'over'
+    continued = 'continued'
+
+
+class Player:
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+
+class Chart:
+
+    def __init__(self):
+        self.top_players = []
+
+    def add(self, candidate):
+        for player in enumerate(self.top_players):
+            if player[1].score < candidate.score:
+                self.top_players.insert(player[0], candidate)
+        if len(self.top_players) > 9:
+            self.top_players.pop()
 
 
 class Game:
@@ -6,19 +32,23 @@ class Game:
     def __init(self, grid):
         self.__grid = grid
         self.__score = 0
+        self.__high_scores = []
+        self.__history = []
+        self.__state = State.running
 
-    def slide_left(self):
-        self.__score += self.grid.slide_left()
-        self.__grid.generate_number()
+    def slide_to(self, direction):
+        if direction not in ['left', 'right', 'up', 'down']:
+            return
+        points_gained = {'left': self.__grid.slide_left,
+                         'right': self.__grid.slide_right,
+                         'up': self.__grid.slide_up,
+                         'down': self.__grid.slide_down
+                         }.get(direction)()
+        self.__score += points_gained
 
-    def slide_right(self):
-        self.__score += self.grid.slide_right()
-        self.__grid.generate_number()
+    def get_value_at(self, position):
+        return self.__grid[position]
 
-    def slide_up(self):
-        self.__score += self.grid.slide_up()
-        self.__grid.generate_number()
-
-    def slide_down(self):
-        self.__score += self.grid.slide_down()
-        self.__grid.generate_number()
+    @property
+    def score(self):
+        return self.__score

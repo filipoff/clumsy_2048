dac848e2b0fa4a7f8ddc6d8b1ae139a46c84b363
Martin <marto_1337@hotmail.com>
2015-07-09 19:55:24 +0300

    chart is now in separate file

--- game.py
+++ game.py
@@ -1,48 +1,12 @@
 from enum import Enum
 from copy import deepcopy
-import json
+from chart import Chart
 
 
 class State(Enum):
     running = 'running'
     game_over = 'game_over'
     continued = 'continued'
-
-
-class Chart:
-
-    def __init__(self):
-        self.top_players = [('', 0) for _ in range(10)]
-
-    def __iter__(self):
-        return iter(self.top_players)
-
-    def is_top_score(self, score):
-        return any(player for player in self.top_players if player[1] < score)
-
-    def save(self, filename):
-        output_file = open(filename, 'w')
-        output_file.truncate()
-        json.dump(self.top_players, output_file)
-        output_file.close()
-
-    def load(self, filename):
-        input_file = open(filename, 'r')
-        self.top_players = json.load(input_file)
-        input_file.close()
-
-    def add(self, name, score):
-        if not self.is_top_score(score):
-            return
-        for player in enumerate(self.top_players):
-            if player[1][1] <= score:
-                self.top_players.insert(player[0], (name, score))
-                break
-        self.top_players.pop()
-
-# TODO finish high_scores
-
-# TODO must finish chart
 
 
 class Game:
